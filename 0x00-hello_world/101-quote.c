3ca15db58e2befb1106919e3a0db667f5dc94737
piccolo <rahimdaud246@gmail.com>
2023-07-13 14:49:29 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program to print a line to the standard error
- * Return 1 (Success)
- */
+ * main - A program that prints a line with puts function
+ ** Return: 0 (Success)
+ **/
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Kora, 2015-10-19\n", 59);
-return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (0);
 }
