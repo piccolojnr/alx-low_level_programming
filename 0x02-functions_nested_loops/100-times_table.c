20067ca871b275198baf4abe971552a420f4aa73
piccolo <rahimdaud246@gmail.com>
2023-07-18 14:37:09 -0700

    a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,40 +14,49 @@ void print_times_table(int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j <= n; j++)
-		{
-			int product = i * j;
-
-			/* Print the appropriate padding and product */
-			if (j == 0)
-				_putchar('0');
-			else if (product < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + product);
-			}
-			else if (product < 100)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + product / 10);
-				_putchar('0' + product % 10);
-			}
-			else
-			{
-				_putchar(' ');
-				_putchar('0' + product / 100);
-				_putchar('0' + (product / 10) % 10);
-				_putchar('0' + product % 10);
-			}
-
-			/* Print separators between products */
-			if (j < n)
-				_putchar(',');
-			else
-				_putchar('\n');
-		}
+		print_times_row(i, n);
 	}
 }
+/**
+ * print_times_row - Prints a single row of the times table.
+ *
+ * @row_num: The row number to be printed.
+ * @max: The maximum value for the times table.
+ */
+void print_times_row(int row_num, int max)
+{
+	for (j = 0; j <= max; j++)
+	{
+		int product = max * j;
+
+		if (j == 0)
+			_putchar('0');
+		else if (product < 10)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('0' + product);
+		}
+		else if (product < 100)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('0' + product / 10);
+			_putchar('0' + product % 10);
+		}
+		else
+		{
+			_putchar(' ');
+			_putchar('0' + product / 100);
+			_putchar('0' + (product / 10) % 10);
+			_putchar('0' + product % 10);
+		}
+
+		if (j < max)
+			_putchar(',');
+		else
+			_putchar('\n');
+	}
+}
+
