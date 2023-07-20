aaa6981c46a87ac5695d6f1036b04ff9c575ff8c
piccolo <rahimdaud246@gmail.com>
2023-07-20 15:42:06 -0700

    betty

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,17 +5,18 @@
  */
 void print_number(int n)
 {
-    unsigned int n1;
-    if (n < 0)
-    {
-        n1 = -n;
-        _putchar('-');
-    }
-    else
-        n1 = n;
+	unsigned int n1;
 
-    if (n1 / 10)
-        print_number(n1 / 10);
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	}
+	else
+		n1 = n;
 
-    _putchar((n1 % 10) + '0');
+	if (n1 / 10)
+		print_number(n1 / 10);
+
+	_putchar((n1 % 10) + '0');
 }
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i != 1)
-			printf(" ");
 		if (i % 3 == 0)
-			printf("Fizz");
+			printf("Fizz ");
 		else if (i % 5 == 0)
-			printf("Buzz");
+			printf("Buzz ");
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else
-			printf("%d", i);
+			printf("%d ", i);
 	}
+
 	printf("\n");
+
 	return (1);
 }
