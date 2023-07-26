4bf1dc1142fb4c44041f5ced3c7612d216f847b5
piccolo <rahimdaud246@gmail.com>
2023-07-26 14:41:40 -0700

    update and fix 101 and 104

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,28 +1,24 @@
 #include "main.h"
+
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
- *
+ * print_number - prints an integer;
+ * @n: integer to be printed;
  */
 void print_number(int n)
 {
-	int divisor = 1;
+	unsigned int n1;
 
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		n = -n;
+	} else
+	{
+		n1 = n;
 	}
 
-	while (n / divisor >= 10)
-	{
-		divisor *= 10;
-	}
+	if (n1 / 10)
+		print_number(n1 / 10);
 
-	while (divisor > 0)
-	{
-		_putchar('0' + n / divisor);
-		n %= divisor;
-		divisor /= 10;
-	}
+	_putchar((n1 % 10) + '0');
 }
--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,47 +1,59 @@
 #include "main.h"
 #include <stdio.h>
-#include <ctype.h>
+/**
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
+ * Return: void
+ */
 
+void print_line(char *c, int s, int l)
+{
+	int j, k;
+
+	for (j = 0; j <= 9; j++)
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
+	}
+}
 /**
  * print_buffer - prints a buffer
- * @b: buffer.
- * @size: size of buffer.
+ * @b: buffer to print
+ * @size: size of buffer
  *
+ * Return: void
  */
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int i;
 
-	if (size <= 0)
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
-		printf("\n");
-		return;
-	}
-
-	for (i = 0; i < size; i += 10)
-	{
-		printf("%08x: ", i);
-
-		for (j = 0; j < 10; j++)
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
 		{
-			if (i + j < size)
-				printf("%02x", (unsigned char)b[i + j]);
-			else
-				printf("  ");
-
-			if (j % 2 != 0)
-				printf(" ");
+			print_line(b, 9, i);
 		}
-
-		printf(" ");
-		for (j = 0; j < 10 && i + j < size; j++)
+		else
 		{
-			if (isprint(b[i + j]))
-				printf("%c", b[i + j]);
-			else
-				printf(".");
+			print_line(b, size % 10 - 1, i);
 		}
-
-		printf("\n");
+		putchar('\n');
 	}
+	if (size == 0)
+		putchar('\n');
 }
