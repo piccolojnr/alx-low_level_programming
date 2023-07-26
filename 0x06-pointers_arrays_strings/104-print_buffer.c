dc8dc17c00690b10502e03fd5f2740dc42b9ed67
Rahim Daud <rahimdaud246@gmail.com>
2023-07-26 18:36:51 +0000

    update 104

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -7,77 +7,34 @@
  * @size: size of buffer.
  *
  */
-void print_buffer(char *b, int size)
-{
-	int i;
-
-	if (size <= 0)
-	{
+void print_buffer(char *b, int size) {
+	if (size <= 0) {
 		printf("\n");
 		return;
 	}
 
-	for (i = 0; i < size; i += 10)
-	{
+	int i, j;
+	for (i = 0; i < size; i += 10) {
 		printf("%08x: ", i);
 
-		print_hexadecimal_content(i, size, b);
+		for (j = 0; j < 10; j++) {
+			if (i + j < size)
+				printf("%02x", (unsigned char)b[i + j]);
+			else
+				printf("  ");
+
+			if (j % 2 != 0)
+				printf(" ");
+		}
 
 		printf(" ");
-
-		print_buffer_content(i,  size, b);
+		for (j = 0; j < 10 && i + j < size; j++) {
+			if (isprint(b[i + j]))
+				printf("%c", b[i + j]);
+			else
+				printf(".");
+		}
 
 		printf("\n");
-	}
-}
-/**
- * print_hexadecimal_content - prints hexadecimal content
- * @i: i
- * @size: size
- * @b: b
- */
-void print_hexadecimal_content(int i, int size, char *b)
-{
-	int j;
-
-	for (j = 0; j < 10; j++)
-	{
-		if (i + j < size)
-		{
-			printf("%02x", (unsigned char)b[i + j]);
-		}
-		else
-		{
-			printf("  ");
-		}
-
-		if (j % 2 == 1)
-		{
-			printf(" ");
-		}
-	}
-}
-/**
- * print_buffer_content - prints buffer content
- * @i: i
- * @size: size
- * @b: b
- */
-void print_buffer_content(int i, int size, char *b)
-{
-	int j;
-
-	for (j = 0; j < 10; j++)
-	{
-		if (i + j < size)
-		{
-			char ch = b[i + j];
-
-			printf("%c", (ch >= 32 && ch <= 126) ? ch : '.');
-		}
-		else
-		{
-			printf(" ");
-		}
 	}
 }
