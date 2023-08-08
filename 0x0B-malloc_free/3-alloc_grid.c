e921c0c7c1fa71c2ec95115c25a915a4cf9de373
rahim daud <rahimdaud246@gmail.com>
2023-08-08 21:13:28 +0000

    man2

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,16 +21,20 @@ int **alloc_grid(int width, int height)
 	if (array == NULL)
 		return (NULL);
 
-	for ( ; height >= 0; height--)
-	{
-		array[height] = (int *)malloc(sizeof(int) * width);
-		if (array[height] == NULL)
-			return (NULL);
-	}
-
 	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < width; i++)
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(array[j]);
+			}
+			free(array);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
 		{
 			array[i][j] = 0;
 		}
