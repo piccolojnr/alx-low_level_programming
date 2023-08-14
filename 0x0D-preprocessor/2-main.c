635f9dc627256cde0048ec5b122984a2fa2ad728
piccolo <rahimdaud246@gmail.com>
2023-08-14 09:47:02 -0700

    third commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
 * main - prints the name of the file it was compiled from
+*
+* Return: 0
 */
-
-int main() {
-    printf("Compiled from file: %s\n", __FILE__);
-    return 0;
+int main(void)
+{
+	printf("Compiled from file: %s\n", __FILE__);
+	return (0);
 }
 
