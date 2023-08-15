7aa47341eb9a3660865035c89617a2d9cac2a0a6
rahim daud <rahimdaud246@gmail.com>
2023-08-15 02:53:04 +0000

    sssas

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,10 +8,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		d = malloc(sizeof(struct dog);
-	d->age = age;
-	d->name = name;
-	d->owner = owner;
+	if (d)
+	{
+		d->age = age;
+		d->name = name;
+		d->owner = owner;
+	}
 }
 
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,9 +7,8 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
-
+	if (d)
+	{
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
@@ -21,5 +20,6 @@ void print_dog(struct dog *d)
 		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
+	}
 }
 
