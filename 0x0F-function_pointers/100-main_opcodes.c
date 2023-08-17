bb00abfcaea4feebf84c07ade57788efce2bcf15
rahim daud <rahimdaud246@gmail.com>
2023-08-17 01:05:30 +0000

    op_code

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,38 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - ..
+ * @argc: ..
+ * @argv: ..
+ *
+ * Return: Alwyas 0 (success)
+ */
 int main(int argc, char *argv[])
 {
-    int numBytes, i;
-    unsigned char *ptr;
+	int numBytes, i;
+	unsigned char *ptr;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    numBytes = atoi(argv[1]);
+	numBytes = atoi(argv[1]);
 
-    if (numBytes < 0)
-    {
-        printf("Error\n");
-        return (2);
-    }
+	if (numBytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
 
-    ptr = (unsigned char *)main;
+	ptr = (unsigned char *)main;
 
-    for (i = 0; i < numBytes; i++)
-    {
-        if (i == numBytes - 1)
-        {
-            printf("%02hhx\n", ptr[i]);
-            break;
-        }
-        printf("%02hhx ", ptr[i]);
-    }
+	for (i = 0; i < numBytes; i++)
+	{
+		if (i == numBytes - 1)
+		{
+			printf("%02hhx\n", ptr[i]);
+			break;
+		}
+		printf("%02hhx ", ptr[i]);
+	}
 
-    printf("\n");
+	printf("\n");
 
-    return (0);
+	return (0);
 }
+
