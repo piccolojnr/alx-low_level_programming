092a4316d61b2047ce7758f6003d1005c9e771a9
rahim daud <rahimdaud246@gmail.com>
2023-08-17 01:06:50 +0000

    op_code 2

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,45 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - ..
- * @argc: ..
- * @argv: ..
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: Alwyas 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int numBytes, i;
-	unsigned char *ptr;
+	int bytes, i;
+	char *arr;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
 
-	numBytes = atoi(argv[1]);
+	bytes = atoi(argv[1]);
 
-	if (numBytes < 0)
+	if (bytes < 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
 
-	ptr = (unsigned char *)main;
+	arr = (char *)main;
 
-	for (i = 0; i < numBytes; i++)
+	for (i = 0; i < bytes; i++)
 	{
-		if (i == numBytes - 1)
+		if (i == bytes - 1)
 		{
-			printf("%02hhx\n", ptr[i]);
+			printf("%02hhx\n", arr[i]);
 			break;
 		}
-		printf("%02hhx ", ptr[i]);
+		printf("%02hhx ", arr[i]);
 	}
-
-	printf("\n");
-
 	return (0);
 }
-
