6e98b0d64ac05975642811330a1a4429a3bdb54b
piccolojnr <99098839+piccolojnr@users.noreply.github.com>
2023-08-29 22:20:00 +0000

    101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,52 @@
 #include "lists.h"
 /**
+ * looped_listint_len - Counts the number of unique nodes
+ *                      in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ *
+ * Return: If the list is not looped - 0.
+ *         Otherwise - the number of unique nodes in the list.
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *slow, *fast;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	slow = head->next;
+	fast = (head->next)->next;
+
+	while (fast)
+	{
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				nodes++;
+				slow = slow->next;
+				fast = fast->next;
+			}
+
+			slow = slow->next;
+			while (slow != fast)
+			{
+				nodes++;
+				slow = slow->next;
+			}
+
+			return (nodes);
+		}
+
+		slow = slow->next;
+		fast = (fast->next)->next;
+	}
+
+	return (0);
+}
+/**
  * print_listint_safe - prints a linked list, safely
  * @head: list of type listint_t to print
  *
@@ -8,20 +55,27 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *current = head;
-	size_t count = 0;
+	size_t count, i = 0;
 
-	while (current != NULL)
+	count = looped_listint_len(head);
+
+	if (count == 0)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
-
-		if (current <= current->next)
+		for (; head != NULL; count++)
 		{
-			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-			exit(98);
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+	}
+	else
+	{
+		for (i = 0; i < count; i++)
+		{
+			printf("[%p] %d\n", (void *)current, current->n);
+			head = head->next;
 		}
 
-		current = current->next;
-		count++;
+		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 
 	return (count);
