0c07abde412034c1cfa9ba968e544cb23edc283c
piccolojnr <99098839+piccolojnr@users.noreply.github.com>
2023-08-29 21:45:48 +0000

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,10 +14,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 
 	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	if (!new_node || !head)
 		return (NULL);
 
 	new_node->n = n;
+	new_node->next = NULL;
 
 	if (idx == 0)
 	{
@@ -30,7 +31,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (current == NULL || current->next == NULL)
 		{
-			free(new_node);
 			return (NULL);
 		}
 
