ae2052f1c4e7e94e9d1bba0ebd05dee3ad042edf
piccolojnr <99098839+piccolojnr@users.noreply.github.com>
2023-08-29 18:01:41 +0000

    kjjj

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,8 +12,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *current = *head;
 	unsigned int i;
 
-	if (head == NULL || *head == NULL)
-		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
