d5fdcec9f7773eb2794af6bdb3e34eb4fefe16ca
piccolo <rahimdaud246@gmail.com>
2023-09-01 01:20:14 +0000

    o again

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,4 @@
 #include "main.h"
-
-unsigned int power(int base, int exp);
-/**
- * power - calculates the power of a number
- * @base: base of the number
- * @exp: exponent of the number
- *
- * Return: the power of the number
- */
-unsigned int power(int base, int exp)
-{
-int i;
-
-unsigned int res = 1;
-
-for (i = 0; i < exp; i++)
-res *= base;
-
-return (res);
-}
-
 /**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: pointer to a string containing a binary number
@@ -28,25 +7,26 @@ return (res);
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int res = 0;
-int len = 0, base = 2, i;
+unsigned int result;
+int i;
 
 if (b == NULL)
+{
 return (0);
-
-while (b[len] != '\0')
-len++;
-
-for (i = len - 1; i >= 0; i--)
-{
-if (b[i] == '1' || b[i] == '0')
-{
-unsigned int v = b[i] - '0';
-res += v * power(base, len - i - 1);
 }
+
+result = 0;
+
+for ( i = 0; b[i] != '\0'; i++)
+{
+if (b[i] == '0') 
+result = (result << 1) | 0;
+else if (b[i] == '1') 
+result = (result << 1) | 1;
 else
-return (0);
+return (0); 
+        
 }
 
-return (res);
+return (result);
 }
