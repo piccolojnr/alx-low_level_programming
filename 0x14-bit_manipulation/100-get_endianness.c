cc2e7c9e4ccd4889e9fd23357b292a4acae6124b
piccolo <rahimdaud246@gmail.com>
2023-09-01 01:41:06 +0000

    adv

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_endianness - checks the endianness
+ * @void: void
+ * 
+ * Return: 0 if big endian, 1 if little endian
+*/
+int get_endianness(void)
+{
+int n = 1;
+char *c = (char *)&n;
+
+if (*c)
+return (1);
+       
+return (0);
+}
