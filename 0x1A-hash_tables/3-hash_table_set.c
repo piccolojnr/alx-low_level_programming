51ff4d57dce96fae5b313e25bdb88f1fd865e056
piccolojnr <rahimdaud246@gmail.com>
2023-11-04 04:43:08 +0000

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,13 +11,12 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 hash_node_t *new_node;
-char *cpy;
+char *v_cpy;
 unsigned long int index, i;
 if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 return (0);
-
-cpy = strdup(value);
-if (cpy == NULL)
+v_cpy = strdup(value);
+if (v_cpy == NULL)
 return (0);
 index = key_index((const unsigned char *)key, ht->size);
 for (i = index; ht->array[i]; i++)
@@ -25,14 +24,14 @@ for (i = index; ht->array[i]; i++)
 if (strcmp(ht->array[i]->key, key) == 0)
 {
 free(ht->array[i]->value);
-ht->array[i]->value = cpy;
+ht->array[i]->value = v_cpy;
 return (1);
 }
 }
 new_node = malloc(sizeof(hash_node_t));
 if (new_node == NULL)
 {
-free(cpy);
+free(v_cpy);
 return (0);
 }
 new_node->key = strdup(key);
@@ -41,7 +40,7 @@ if (new_node->key == NULL)
 free(new_node);
 return (0);
 }
-new_node->value = cpy;
+new_node->value = v_cpy;
 new_node->next = ht->array[index];
 ht->array[index] = new_node;
 return (1);
